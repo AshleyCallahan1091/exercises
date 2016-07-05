4bdffc64e814f0edf7a854469fd8e0af6bcc4a2b
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-05 12:43:10 -0500

    added working increment counter and made it so only numbers can be inserted

--- for.php
+++ for.php
@@ -6,18 +6,33 @@ $startingNumber = trim(fgets(STDIN));
 fwrite(STDOUT, "Enter an ending number: \n");
 $endingNumber = trim(fgets(STDIN));
 
+if (is_numeric($startingNumber) && is_numeric($endingNumber) && $startingNumber < $endingNumber) {
+	
+} else {
+	fwrite(STDERR, "Give a number value, you walnut");
+	exit(1);
+}
+
 fwrite(STDOUT, "Enter a counting increment: \n");
 $increment = trim(fgets(STDIN));
+
+if($increment == 0 || $increment == ""){
+	$increment = 1;
+}
 
 while ($endingNumber < $startingNumber) {
 	$endingNumber = 0;
 	fwrite(STDOUT, "Enter an ending number: \n");
 	$endingNumber = trim(fgets(STDIN));
-
 }
 
 for ($i = $startingNumber; $i < $endingNumber; $i++) {
+	if($i%$increment == 0){
+
 	fwrite(STDOUT, "$i \n");
+	}
+
+
 }
 
 
