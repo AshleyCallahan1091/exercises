82d4c6a0b07e8ef7332f65e45a6208c0be385aeb
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-05 14:50:13 -0500

    Changed it to show scalar items

--- foreach.php
+++ foreach.php
@@ -3,17 +3,7 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-    if (is_bool($thing)) {
-        echo "{$thing} is a boolean\n";
-    } elseif (is_string($thing)) {
-        echo "{$thing} is a string\n";
-    } elseif (is_int($thing)){
-    	echo "{$thing} is a number\n";
-    } elseif (is_float($thing)){
-    	echo "{$thing} is a decimal number\n";
-    }elseif (is_null($thing)){
-    	echo "{$thing} is a null value\n";
-    }elseif (is_array($thing)){
-    	echo "{$thing} is an array\n";
+    if (is_scalar($thing)){
+    	echo "{$thing} is scalar\n";
     }
 }
