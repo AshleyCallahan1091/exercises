ed0db71af63b2820bb59659602ec3c4b8f3f178b
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-06 10:56:16 -0500

    Class exercises

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+   	return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+echo add(50, 40);
+echo subtract(50, 40);
+echo multiply(50, 40);
+echo divide(50, 40);
+echo modulus(50, 40);
