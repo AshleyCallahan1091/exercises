61583623fed4d0f0fe6dd687b1568a5e73d66a63
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-06 11:05:21 -0500

    finished math exercise

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 1;
+$b = 2;
+
 function add($a, $b)
 {
     return $a + $b;
@@ -20,8 +23,19 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+
+
 echo add(50, 40);
+echo "\n";
 echo subtract(50, 40);
+echo "\n";
 echo multiply(50, 40);
+echo "\n";
 echo divide(50, 40);
+echo "\n";
 echo modulus(50, 40);
+echo "\n";
