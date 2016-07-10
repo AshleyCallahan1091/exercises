b356d4ed13b09b2b6daa1b1880e0c3b7dee89d97
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-10 17:04:38 -0500

    showed the same names from both arrays

--- search-arrays.php
+++ search-arrays.php
@@ -4,10 +4,22 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$result = array_search('Bob', $compare);
+//$result = array_search('Bob', $compare);
 
-if ($result === FALSE){
-	echo "FALSE \n";
-} else {
-    echo "TRUE \n";
+// if ($result === FALSE){
+// 	return "FALSE \n";
+// } else {
+//     return "TRUE \n";
+// }
+
+
+
+function searchNames ($names, $compare) {
+	foreach ($names as $name) {
+    	$result = array_search($name, $compare);
+        $same[] = $result;
+	}
+	return print_r($same);
+
 }
+return searchNames($names, $compare);
