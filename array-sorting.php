b08ad23eb5d3373f6155e831ae3f5f32d7942922
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-12 11:53:22 -0500

    classroom exercises

---
+++ array-sorting.php
@@ -0,0 +1,33 @@
+<?php 
+
+$array = array('e', 'a', 'g', 'c', 'i', 'd', 'f', 'b', 'h');
+
+print_r($array);
+
+$count = count($array);
+
+for ($a = 0; $a < $count; $a++) {
+    for ($b = $a + 1; $b < $count; $b++) {
+        if ($array[$a] > $array[$b]) {
+            $filler = $array[$a];
+            $array[$a] = $array[$b];
+            $array[$b] = $filler;
+        }
+    }
+}
+
+print_r($array);
+
+
+$unsorted = $array;
+$sorted = [];
+
+foreach ($array as $i => $letter) {
+	$min = min($unsorted);
+	$sorted[] = $min;
+	$index = array_search($min, $unsorted);
+	unset($unsorted[$index]);
+
+}
+
+print_r($sorted);
---
+++ logger.php
@@ -0,0 +1,28 @@
+<?php
+
+
+date_default_timezone_set('America/Chicago');
+
+function logMessage($logLevel, $message)
+{
+  	$filename = "log-" . date('Y-m-d') . '.log';
+  	$handle = fopen($filename, 'a');
+
+  	$formattedMessage = date('Y-m-d H:i:s') . "[$logLevel] $message";
+  	fwrite($handle, $formattedMessage);
+}
+
+
+function logInfo($message) {
+	logMessage('INFO', $message);
+}
+
+function logError($message){
+	logMessage('ERROR', $message);
+
+}
+
+
+logInfo("I'm with you til the end of the line.");
+logError("Hail Hydra.");
+
