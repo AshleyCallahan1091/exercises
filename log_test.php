23117d2c190c4e297c844b864b824e34548b8839
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-22 11:58:51 -0500

    logger assignments

---
+++ log_test.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once 'src/log.php';
+
+$logOne = new Log();
+
+$logOne->info("I'm with you til the end of the line.");
+
+$logOne->error("Hail Hydra.");
---
+++ src/log.php
@@ -0,0 +1,31 @@
+<?php class Log {
+
+	public $filename;
+	public $handle;
+	
+
+
+	public function __construct($prefix = "log-") {
+		$this->filename = $prefix . date('Y-m-d') . '.log';
+		$this->handle = fopen($this->filename, 'a');
+	}
+	
+	public function logMessage($logLevel, $message) {
+		$formattedMessage = date('Y-m-d') . "[$logLevel] $message";
+		fwrite($this->handle, $formattedMessage);
+	}
+
+	public function __destruct(){ 
+		fclose($this->handle);
+			
+	}
+
+	public function info($message) {
+		$this->logMessage('INFO', $message);
+	}
+
+	public function error($message){
+		$this->logMessage('ERROR', $message);
+	}
+
+}
